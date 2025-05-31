a37cae2cd6d62f48877598f58a42fa6c1981812e
rladus <knyeon26@gmail.com>
2025-05-31 13:18:55 +0900

    feat: 레이아웃 설정

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,6 +1,3 @@
-import { MdHome } from 'react-icons/md';
-import Link from 'next/link';
-
 export const Header = () => {
   return (
     <header className='flex h-[64px] w-full flex-col items-center justify-center border-b border-gray-800'>
