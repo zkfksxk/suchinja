c1e4b45e5ac72f1f0caa17656a2928a38c69e745
rladus <knyeon26@gmail.com>
2025-06-30 14:19:16 +0900

    feat: home 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import { Button } from '@mantine/core';
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <main className='w-full flex flex-1 flex-col items-center justify-center border p-5'>
+    <main
+      className={`w-full flex flex-1 flex-col items-center justify-center border p-5`}
+      style={{ background: currentIndex === 0 ? '#0077F6' : '#fff' }}
+    >
       {currentIndex === 0 && (
-        <div className='flex flex-col'>
+        <div className='flex flex-col gap-7'>
+          <Image
+            src='/images/home.png'
+            alt='home image'
+            width={400}
+            height={400}
+            className='rounded-xl'
+          />
           <Button
             variant='filled'
             color='blue.9'
             size='xl'
-            className='fixed bottom-5'
+            radius='md'
             onClick={() => setCurrentIndex(1)}
           >
             시작하기
--- shared/constant.ts
+++ shared/constant.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
   title: '수친자 테스트',
-  description: '내가 수영에 미친 정도는?',
+  description: '내가 수영에 사랑하는 정도는?',
 };
 
 export const QUESTION_COUNT = 10;
