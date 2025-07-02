cd91e076903f59270af88d2d9dbbdb01eec1bfae
rladus <knyeon26@gmail.com>
2025-07-02 17:02:46 +0900

    feat: 질문 3개 추가

--- shared/constant.ts
+++ shared/constant.ts
@@ -10,10 +10,28 @@ export const QUESTION_COUNT = 10; //12~14까지 늘리기
 export const questionList: Questions = [
   {
     id: 1,
-    question: '방학이 끝나고, 드디어 새학기가 되었다! 나의 기분은?',
+    question: '선호하는 시간대는?',
     answers: [
-      { id: 1, text: '설레면서도 잘 지낼 수 있을까 걱정된다.' },
-      { id: 2, text: '새로운 친구들을 만날 생각에 너무 기대된다.' },
+      { id: 1, text: '아침' },
+      { id: 2, text: '저녁' },
+      { id: 3, text: '그런 걸 따지다니. 언제든 옳아' },
+    ],
+  },
+  {
+    id: 2,
+    question: '수영장 가기 직전, 내 기분은?',
+    answers: [
+      { id: 1, text: '설레서 시간에 맞추어 준비한다.' },
+      { id: 2, text: '밍기적거리다가 아슬아슬하게 도착한다.' },
+      { id: 3, text: '오늘은 피곤하니 스킵~' },
+    ],
+  },
+  {
+    id: 3,
+    question: '수영을 못 가는 날이 생기면?',
+    answers: [
+      { id: 1, text: '아쉬워서 계속 생각난다.' },
+      { id: 2, text: '다음에 가지 뭐 하고 넘긴다.' },
     ],
   },
 ];
